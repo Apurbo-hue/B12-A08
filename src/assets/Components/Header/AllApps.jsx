8dded87993b385c8eec7dd7b973a25fde0611218
Apurbo-hue <labunna123@gmail.com>
2026-01-10 01:22:01 +0600

    search added

--- src/assets/Components/Header/AllApps.jsx
+++ src/assets/Components/Header/AllApps.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router';
 import AppsCard from '../Apps/AppsCard';
+import errorImg from './../../images/App-Error.png'
 
 const AllApps = () => {
     const allData = useLoaderData();
+    const [search, setSearch] = useState("")
+
+
+    const filteredApps = allData.filter(app => app.title.toLowerCase().includes(search.toLowerCase()))
+    console.log(filteredApps)
 
     return (
         <div>
@@ -11,12 +17,25 @@ const AllApps = () => {
                 <h1 className='text-4xl font-bold mb-4'>Our All Applications</h1>
                 <p>Explore All Apps on the Market developed by us. We code for Millions</p>
             </div>
-            <div>
-                <h1>({allData.length}) Apps found</h1>
+            <div className='flex justify-between'>
+                <h1 className='p-4'>({allData.length}) Apps found</h1>
+                <input type="text" placeholder='Search Apps' className='pl-4 pr-12 border'
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)} />
             </div>
+            
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 place-items-center gap-6 sm:justify-items-stretch'>
-                {allData.map((apps) => <AppsCard key={apps.id} apps={apps}></AppsCard>)}
+                {filteredApps.map((apps) => <AppsCard key={apps.id} apps={apps}></AppsCard>)}
+
             </div>
+            {filteredApps.length === 0 && search.trim() !== "" && (
+                <div className='flex justify-center'>
+                    <figure>
+                        <img src={errorImg} alt="" />
+                    </figure>
+                </div>
+
+            )}
         </div>
     );
 };
